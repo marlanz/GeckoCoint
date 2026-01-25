d79e007fd9942ff8254733b2c29366ba35fabb73
marlanz <phucanhdodang1211@gmail.com>
2026-01-26 00:40:39 +0700

    feat(ui): add converter component

--- components/LiveDataWrapper.tsx
+++ components/LiveDataWrapper.tsx
@@ -6,6 +6,7 @@ import { useCoinGeckoWebSocket } from "@/hooks/useCoinGeckoWebSocket";
 import DataTable from "@/components/DataTable";
 import { formatCurrency, timeAgo } from "@/lib/utils";
 import { useState } from "react";
+import CoinHeader from "./CoinHeader";
 // import CoinHeader from "@/components/CoinHeader";
 
 const LiveDataWrapper = ({
@@ -58,19 +59,18 @@ const LiveDataWrapper = ({
 
   return (
     <section id="live-data-wrapper">
-      {/* <CoinHeader
+      <CoinHeader
         name={coin.name}
         image={coin.image.large}
-        livePrice={price?.usd ?? coin.market_data.current_price.usd}
+        livePrice={coin.market_data.current_price.usd}
         livePriceChangePercentage24h={
-          price?.change24h ??
           coin.market_data.price_change_percentage_24h_in_currency.usd
         }
         priceChangePercentage30d={
           coin.market_data.price_change_percentage_30d_in_currency.usd
         }
         priceChange24h={coin.market_data.price_change_24h_in_currency.usd}
-      /> */}
+      />
       <Separator className="divider" />
 
       <div className="trend">
